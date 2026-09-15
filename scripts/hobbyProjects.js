a19554d26b81ecd56425742249e65c25dd7c19a1
reybd03 <rdbenavidez.08@gmail.com>
2026-09-15 11:20:26 -0500

    Updates to hobby image loading

--- scripts/hobbyProjects.js
+++ scripts/hobbyProjects.js
@@ -7,11 +7,15 @@ let imageIndex = 1;
 function loadImages() {
     const img = new Image();
     for (const name of imageNames) {
-        
+
         img.src = `${folderPath}${name}_${imageIndex}.jpg`; // Assumes names like 1.jpg, 2.jpg
-        console.log("image source: " +img.src);
+        img.style.width = '200px';
+        img.style.height = '150px';
+        img.style.objectFit = 'cover';
+
+        console.log("image source: " + img.src);
         // If the image exists, append it and try the next one
-        img.onload = function() {
+        img.onload = function () {
             container.appendChild(img);
             imageIndex++;
             loadImages(); // Recursive call to check next image
